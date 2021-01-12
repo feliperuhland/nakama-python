110a8e678a37c87a3a7798b833728e7a69f61cb8
Felipe Ruhland <felipe.ruhland@gmail.com>
2021-01-12 22:02:32 +0100

    Readme and HttpClient

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # nakama
 Python client for Nakama's server
+
+# WIP
+
+- [ ] GET /v2/console/account
+- [ ] DELETE /v2/console/account
+- [ ] GET /v2/console/account/{id}
+- [ ] DELETE /v2/console/account/{id}
+- [ ] POST /v2/console/account/{id}
+- [ ] POST /v2/console/account/{id}/ban
+- [ ] GET /v2/console/account/{id}/export
+- [ ] GET /v2/console/account/{id}/friend
+- [ ] DELETE /v2/console/account/{id}/friend/{friendId}
+- [ ] GET /v2/console/account/{id}/group
+- [ ] DELETE /v2/console/account/{id}/group/{groupId}
+- [ ] POST /v2/console/account/{id}/unban
+- [ ] POST /v2/console/account/{id}/unlink/apple
+- [ ] POST /v2/console/account/{id}/unlink/custom
+- [ ] POST /v2/console/account/{id}/unlink/device
+- [ ] POST /v2/console/account/{id}/unlink/email
+- [ ] POST /v2/console/account/{id}/unlink/facebook
+- [ ] POST /v2/console/account/{id}/unlink/facebookinstantgame
+- [ ] POST /v2/console/account/{id}/unlink/gamecenter
+- [ ] POST /v2/console/account/{id}/unlink/google
+- [ ] POST /v2/console/account/{id}/unlink/steam
+- [ ] GET /v2/console/account/{id}/wallet
+- [ ] DELETE /v2/console/account/{id}/wallet/{walletId}
+- [ ] GET /v2/console/api/endpoints
+- [ ] POST /v2/console/api/endpoints/rpc/{method}
+- [ ] POST /v2/console/api/endpoints/{method}
+- [X] POST /v2/console/authenticate
+- [X] GET /v2/console/config
+- [ ] GET /v2/console/leaderboard
+- [ ] GET /v2/console/leaderboard/{id}
+- [ ] DELETE /v2/console/leaderboard/{id}
+- [ ] GET /v2/console/leaderboard/{leaderboardId}/records
+- [ ] GET /v2/console/match
+- [ ] GET /v2/console/match/{id}/state
+- [ ] GET /v2/console/runtime
+- [ ] GET /v2/console/status
+- [ ] GET /v2/console/storage
+- [ ] DELETE /v2/console/storage
+- [ ] GET /v2/console/storage/collections
+- [ ] GET /v2/console/storage/{collection}/{key}/{userId}
+- [ ] DELETE /v2/console/storage/{collection}/{key}/{userId}
+- [ ] PUT /v2/console/storage/{collection}/{key}/{userId}
+- [ ] DELETE /v2/console/storage/{collection}/{key}/{userId}/{version}
+- [ ] GET /v2/console/user
+- [ ] DELETE /v2/console/user
+- [ ] POST /v2/console/user
--- nakama/__init__.py
+++ nakama/__init__.py
@@ -3,12 +3,29 @@ import json
 import urllib.request
 
 
-DEFAULT_VERSION = 2
+DEFAULT_VERSION = "v2"
 
 
 class HttpMethods(enum.Enum):
     GET = "GET"
     POST = "POST"
+
+
+class HttpClient:
+    def _request(self, url, data=None, headers=None, method=HttpMethods.GET):
+        headers = headers or {}
+        data = data or {}
+
+        req = urllib.request.Request(url, json.dumps(data).encode(), headers, method=method.value)
+        res = urllib.request.urlopen(req)
+        payload = json.loads(res.read().decode())
+        return payload, res.code
+
+    def get(self, url, headers=None):
+        return self._request(url, headers=headers, method=HttpMethods.GET)
+
+    def post(self, url, data=None, headers=None):
+        return self._request(url, data, headers=headers, method=HttpMethods.POST)
 
 
 class NakamaConsoleClient:
@@ -19,31 +36,20 @@ class NakamaConsoleClient:
         self.ssl = kwargs.get("ssl", True)
         self.version = kwargs.get("version", DEFAULT_VERSION)
         self.token = None
-
-    def _http_request(self, url, data, headers=None, method=HttpMethods.GET):
-        headers = headers or {}
-        if self.token:
-            headers["Authorization"] = f"Bearer {self.token}"
-
-        req = urllib.request.Request(url, json.dumps(data).encode(), headers, method=method.value)
-        res = urllib.request.urlopen(req)
-        payload = json.loads(res.read().decode())
-        return payload
+        self.http_client = HttpClient()
 
     def authenticate(self, username: str, password: str) -> str:
         data = {"username": username, "password": password}
-        payload = self._http_request(
-            f"http://{self.host}:{self.port}/v2/console/authenticate",
+        payload, status_code = self.http_client.post(
+            f"http://{self.host}:{self.port}/{self.version}/console/authenticate",
             data,
-            method=HttpMethods.POST,
         )
         self.token = payload["token"]
         return self.token
 
     def config(self):
-        payload = self._http_request(
-            f"http://{self.host}:{self.port}/v2/console/config",
-            {},
-            method=HttpMethods.GET,
+        payload, status_code = self.http_client.get(
+            f"http://{self.host}:{self.port}/{self.version}/console/config",
+            headers={"Authorization": f"Bearer {self.token}"},
         )
         return payload
